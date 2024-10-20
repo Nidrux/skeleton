f9c07be3ef996df2de4fa0b71913f75715446a94
Nidrux <37250273+Nidrux@users.noreply.github.com>
2024-10-20 16:04:52 +0200

    feat: add status

--- src/Handlers/ClientHandler.ts
+++ src/Handlers/ClientHandler.ts
@@ -1,4 +1,4 @@
-import { Client } from "discord.js";
+import { ActivityType, Client } from "discord.js";
 import { clientOptions } from "../Extensions/ClientOptions";
 import { fileHandler, log } from "..";
 
@@ -6,16 +6,17 @@ import { fileHandler, log } from "..";
 export default class ClientHandler extends Client {
     _token : string = ""
     constructor(token : string = "") {
-        super(clientOptions);
-        this._token = token;
+        super(clientOptions)
+        this._token = token
     }
     public start(): void {
         log.info(`Starting up bot.`)
         try {
             const isDataDirectoryCreated = fileHandler.ensureDataDirectoryCreated()
             if(isDataDirectoryCreated.succes) {
-                log.info(isDataDirectoryCreated.result);
-                this.login(this._token)   
+                log.info(isDataDirectoryCreated.result)
+                this.login(this._token)
+                this.setClientStatus()   
             } else {
                 log.error(`Failed to create Data directory before starting up the bot. Error: ${isDataDirectoryCreated.result}`)
                 process.exit(1);
@@ -24,5 +25,14 @@ export default class ClientHandler extends Client {
             log.fatal(`Failed to login.. With error message: ${error}. Terminating PROC`)
             process.exit(1);
         }
+    }
+    private setClientStatus() : void {
+        this.user?.setPresence({
+            activities: [{ 
+                name: 'Living on a random cluster', 
+                type: ActivityType.Watching
+            }], 
+            status: 'online' 
+        })
     }
 }
